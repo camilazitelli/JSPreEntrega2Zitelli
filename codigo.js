a78b89f5ba4d89b3b94dcdc9eb3bdf2cfa013986
camilazitelli <camilazitelli@gmail.com>
2023-02-11 18:01:25 -0300

    codeamos con html, css y js

--- codigo.js
+++ codigo.js
@@ -1,86 +1,20 @@
-///catalogo de bebidas
-const todasLasBebidas = [
-    {nombre: 'Gin', precio: "2500"},
-    {nombre: 'Fernet', precio: "1900"},
-    {nombre: 'Aperol', precio: "1700"},
-    {nombre: 'Cynar', precio: "1500"},
-    {nombre: 'Gancia', precio: "900"},
-]
-
-///Menu
-const Menu = ["1-Buscar bebida por nombre","2-Conocer el catalogo completo, con descuento por pago con transferencia", "3-La bebida que estoy buscando no está disponible, me gustaria encargarla", "4-Salir"]
-
-///calcular descuento por pago en transferencia
-function descuentosPrecios (){
-alert("A continuacion le mostramos los precios con descuento del 10% por abonar por transferencia o en efectivo");
-const descuentoTransferencia = todasLasBebidas.map((el) => {
-    return {
-        nombre: el.nombre,
-        precio: el.precio - 1.10
-    }
-})
-alert(JSON.stringify(descuentoTransferencia));}
-
-
-//// buscar la bebida
-function buscarBebidaPorNombre (){
-    let busqueda = prompt("Ingrese el nombre de la bebida que está buscando")
-            let resultado = todasLasBebidas.find(
-            (el) => el.nombre.toLowerCase() == busqueda.toLowerCase()
-            )
-                if(resultado == undefined){
-                alert(JSON.stringify(`${busqueda} no se encuentra en nuestro stock`))
-                }else{
-alert(JSON.stringify(resultado))
+let miFormulario = document.getElementById("formulario");
+miFormulario.addEventListener("submit", validarFormulario);
+function validarFormulario(e){
+e.preventDefault();
+let formulario = e.target
+sessionStorage.setItem('nombre', formulario.children[0].value); 
+sessionStorage.setItem('apellido', formulario.children[1].value);
+sessionStorage.setItem('telefono', formulario.children[2].value); 
+sessionStorage.setItem('bebida pedida', formulario.children[3].value); 
+const nombre = sessionStorage.getItem('nombre');
+const apellido = sessionStorage.getItem('apellido');
+const telefono = sessionStorage.getItem('telefono');
+const bebidaPedida = sessionStorage.getItem('bebida pedida');
+let parrafo = document.createElement('div');
+parrafo.innerText = 'Gracias por tu pedido ' + nombre + " " + apellido + "." + " " + "Cuando tengamos existencias de " + bebidaPedida + " nos comunicaremos al " + telefono + "."
+document.body.append(parrafo);
 }
-}
-
-///encargar 
-function realizarEncargue (){
-    const nombreComprador = []
-    const nombreBebidaPedida = []
-    const numeroDeContacto = []
-    }
-
-///resultado del encargue    
-function resumenEncargue (){
-    const nombreBebidaPedida = prompt("Ingrese el nombre de la bebida");
-    const nombreComprador = prompt("Ingrese su nombre");
-    const numeroDeContacto = prompt("Ingrese su numero de contacto");
-    alert("Gracias por su pedido " + nombreComprador + "." + " Cuando tengamos existencias de " + nombreBebidaPedida + " nos comunicaremos al " + numeroDeContacto)}
-
-
-let entrada = prompt("Ingrese su edad");
-if (entrada < "18"){
-    alert("Lo sentimos, no puede continuar");
-}else{ (entrada >= "18");{
-    alert("Bienvenido a la tienda de Corchos & Barriles");
-    Menu.forEach((item)=> {
-        alert(item)
-    })
-    let opciones = prompt("Ingrese una opcion del menu");
-    while(opciones != "ESC"){
-    switch (opciones) {
-        case "1":
-            buscarBebidaPorNombre();
-            break;        
-        case "2":
-            descuentosPrecios();
-            break;
-        case "3":
-            realizarEncargue();
-            resumenEncargue();
-            break;
-        case "4":
-            alert("Gracias por utilizar nuestra pagina")
-            break;    
-        default:
-            alert("Ingrese una opcion válida")    
-                }
-            opciones = prompt("Ingrese una opcion del menu");
-            }
-        }
-    }
 
 
 
